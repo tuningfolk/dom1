30a945a7fa0045cc14c298b06eabb20e49241593
tuningfolk <alisahadges@gmail.com>
2023-01-14 19:02:16 +0530

    Added h3

--- js-file.js
+++ js-file.js
@@ -9,7 +9,7 @@ container.appendChild(p);
 //second
 const h3 = document.createElement('h3');
 h3.style.color = 'blue';
-h3.textContent = "I'm a blue h3!"
+h3.textContent = "I'm a blue h3!";
 
-container.appendChild(h3)
+container.appendChild(h3);
 
