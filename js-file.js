6c7ddff77ee426c34e30a0fe78ef83684dffb9e5
tuningfolk <alisahadges@gmail.com>
2023-01-14 18:54:35 +0530

    Added h3

--- js-file.js
+++ js-file.js
@@ -1,6 +1,13 @@
-const p = document.createElement('p')
-p.style.color = 'red'
-p.textContent = "Hey! I'm red!"
+//first
+const p = document.createElement('p');
+p.style.color = 'red';
+p.textContent = "Hey I'm red!";
 
-const container = document.querySelector('#container')
-container.appendChild(p)
+const container = document.querySelector('#container');
+container.appendChild(p);
+
+//second
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = "I'm a blue h3!"
+
